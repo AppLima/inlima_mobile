cc6b79e3aad766998070330394ef72f31e250fd2
RetLight <renzotipula2703@gmail.com>
2024-09-27 11:17:00 -0500

    Folders

---
+++ lib/configs/colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primaryColorInlima = Color(0xFF2B4C7E);
+  static const Color secondaryColorInlima = Color(0xFF567EBB);
+  static const Color tertiaryColorInlima = Color(0xFF606D80);
+  static const Color blackInlima = Color(0xFF201F20);
+  static const Color lightGreyInlima = Color(0xFFDCE0E6);
+  static const Color textColorWhite = Color(0xFFFFFFFF);
+  static const Color textColorBlack = Color(0x00000000);
+  static const Color textColorLightGray = Color(0xFFA9A9A9); 
+}
+  
--- lib/main.dart
+++ lib/main.dart
@@ -1,90 +1,33 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_storage/firebase_storage.dart';
-import 'package:image_picker/image_picker.dart';
-import 'dart:io';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  runApp(MyApp());
+void main(){
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp();
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: ImageUploader(),
-    );
-  }
-}
-
-class ImageUploader extends StatefulWidget {
-  @override
-  _ImageUploaderState createState() => _ImageUploaderState();
-}
-
-class _ImageUploaderState extends State<ImageUploader> {
-  File? _image;
-  final picker = ImagePicker();
-
-  // URL de la imagen en Firebase Storage
-  final String imageUrl = 'https://firebasestorage.googleapis.com/v0/b/inlima-snapcloud.appspot.com/o/quejas%2Ftraka.jpg?alt=media&token=03805517-5ecb-4cdd-b495-9264ba3437bc';
-
-  Future<void> _pickImage() async {
-    final pickedFile = await picker.pickImage(source: ImageSource.gallery);
-
-    setState(() {
-      if (pickedFile != null) {
-        _image = File(pickedFile.path);
-      } else {
-        print('No image selected.');
-      }
-    });
-  }
-
-  Future<void> _uploadImage() async {
-    if (_image != null) {
-      try {
-        String fileName = _image!.path.split('/').last;
-        FirebaseStorage storage = FirebaseStorage.instance;
-        Reference ref = storage.ref().child('images/$fileName');
-        await ref.putFile(_image!);
-        print('Image uploaded successfully');
-      } catch (e) {
-        print('Failed to upload image: $e');
-      }
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Image Uploader'),
+      title: 'InLima',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.white),
+        useMaterial3: true,
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            // Mostrar la imagen desde Firebase Storage usando la URL
-            Image.network(imageUrl),
-            SizedBox(height: 20),
-            _image != null
-                ? Image.file(_image!)
-                : Text('No image selected.'),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: _pickImage,
-              child: Text('Pick Image'),
-            ),
-            ElevatedButton(
-              onPressed: _uploadImage,
-              child: Text('Upload Image'),
-            ),
-          ],
-        ),
-      ),
+      
+        home: Scaffold(
+          appBar: AppBar(title: Text('Inlima')),
+          body: Text("Hola mundo"),
+        )
+
+      //initialRoute: '/sign-in',
+
+      //routes: {
+        //'/home':  (context) => HomePage(),
+        //'/sign-in': (context) => SignInPage(),
+        //'/sign-up': (context) => SignUpPage(),
+        //'/reset': (context) => ResetPage(),
+      //};
     );
   }
 }
