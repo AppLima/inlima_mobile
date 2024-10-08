2e6e954c9b69b0f3d333f5d929f63068f626a651
RLJ17 <20201192@aloe.ulima.edu.pe>
2024-10-08 18:36:12 -0500

    Fix login

--- lib/services/usuario_service.dart
+++ lib/services/usuario_service.dart
@@ -1,73 +1,30 @@
 import 'dart:convert';
-import 'dart:io';
-import 'package:path_provider/path_provider.dart';
+import 'package:flutter/services.dart';
 import 'package:inlima_mobile/models/usuario.dart';
 
 class UsuarioService {
-  final String _fileName = 'usuarios3.json';
+  final String _file = 'assets/json/usuarios.json';
 
-  // Obtiene todos los usuarios desde el archivo
   Future<List<Usuario>> fetchAll() async {
-    final file = await _getFile();
-    if (!await file.exists()) {
-      return [];
-    }
-    final String content = await file.readAsString();
-    final List<dynamic> data = jsonDecode(content);
-    return data.map((usuario) => Usuario.fromMap(usuario)).toList();
+    List<Usuario> usuarios = [];
+    final String response = await rootBundle.loadString(_file);
+    final List<dynamic> data = jsonDecode(response);
+    usuarios = data.map((map) => Usuario.fromMap(map as Map <String, dynamic>)).toList();
+    return usuarios;
   }
 
-  // Agrega un nuevo usuario al archivo
-  Future<void> addUsuario(Usuario usuario) async {
-    final file = await _getFile();
-    List<Usuario> usuarios = await fetchAll();
-    usuarios.add(usuario);
-    String updatedJson = jsonEncode(usuarios.map((usuario) => usuario.toJson()).toList());
-    await file.writeAsString(updatedJson);
-  }
-
-  // Comprueba si un correo ya está registrado
   Future<bool> isEmailAlreadyRegistered(String email) async {
     List<Usuario> usuarios = await fetchAll();
     return usuarios.any((usuario) => usuario.email == email);
   }
 
-  // Busca un usuario por su correo electrónico
   Future<Usuario> findByEmail(String email) async {
     List<Usuario> usuarios = await fetchAll();
-    return usuarios.firstWhere((usuario) => usuario.email == email, orElse: () => throw Exception('Usuario no encontrado'));
+    return usuarios.firstWhere((usuario) => usuario.email == email);
   }
 
-  // Genera un nuevo ID para un usuario
-  Future<int> getNewId() async {
+    Future<int> getNewId() async {
     List<Usuario> usuarios = await fetchAll();
     return usuarios.isNotEmpty ? usuarios.last.idUsuario + 1 : 1;
-  }
-
-  // Elimina un usuario por su ID
-  Future<void> deleteUsuario(int idUsuario) async {
-    final file = await _getFile();
-    List<Usuario> usuarios = await fetchAll();
-    usuarios.removeWhere((usuario) => usuario.idUsuario == idUsuario);
-    String updatedJson = jsonEncode(usuarios.map((usuario) => usuario.toJson()).toList());
-    await file.writeAsString(updatedJson);
-  }
-
-  // Actualiza la información de un usuario
-  Future<void> updateUsuario(Usuario usuarioActualizado) async {
-    final file = await _getFile();
-    List<Usuario> usuarios = await fetchAll();
-    int index = usuarios.indexWhere((usuario) => usuario.idUsuario == usuarioActualizado.idUsuario);
-    if (index != -1) {
-      usuarios[index] = usuarioActualizado;
-      String updatedJson = jsonEncode(usuarios.map((usuario) => usuario.toJson()).toList());
-      await file.writeAsString(updatedJson);
-    }
-  }
-
-  // Obtiene el archivo JSON donde se almacenan los usuarios
-  Future<File> _getFile() async {
-    final directory = await getApplicationDocumentsDirectory();
-    return File('${directory.path}/$_fileName');
   }
 }
