05cd5a5914fc62eafcfe2567e238669f97e71f05
Rafa700 <20200349@aloe.ulima.edu.pe>
2024-10-08 19:51:40 -0500

    (navbar)

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -1,21 +1,70 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 import 'package:inlima_mobile/models/usuario.dart';
+import '../sos/sos_page.dart';
+import '../complaint/complaint_page.dart';  // Import for Complaint Page
+import '../survey/survey.dart';       // Import for Survey Page
 import 'home_controller.dart';
+import '../../components/inlima_appbar.dart';
+import '../../configs/colors.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   final Usuario usuario;
 
   HomePage({required this.usuario});
 
   @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _selectedIndex = 0;
+
+  // List of pages connected to the bottom nav bar
+  final List<Widget> _pages = [
+    ComplaintPage(),               // Complaint page
+    SOSPage(),                    // SOS page
+    SurveyPage(),                  // Survey page
+    Center(child: Text('Historial')), // Placeholder for Historial
+  ];
+
+  // Update selected tab index when a tab is tapped
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Bienvenido, ${usuario.nombre}'),
+        title: Text('Bienvenido, ${widget.usuario.nombre}'),
       ),
-      body: Center(
-        child: Text('Has iniciado sesión como ${usuario.email}'),
+      body: _pages[_selectedIndex], // Display the selected page
+      bottomNavigationBar: BottomNavigationBar(
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.feedback),
+            label: 'Quejas/Sugerencias',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.warning),
+            label: 'SOS',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.poll),
+            label: 'Sondeos',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.history),
+            label: 'Historial',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: AppColors.primaryColorInlima,  // Highlighted color for the selected tab
+        unselectedItemColor: AppColors.lightGreyInlima,     // White icons for unselected tabs
+        backgroundColor: AppColors.blackInlima,    // Dark blue background for navbar
+        onTap: _onItemTapped,                               // Tap handler to switch tabs
       ),
     );
   }
